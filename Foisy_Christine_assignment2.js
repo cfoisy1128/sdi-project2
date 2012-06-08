2a46ae87a8e9008fec06c563ff1983968a6501ba
Christine Foisy <christinefoisy@new-host-2.home>
2012-06-08 12:59:01 -0400

    added files to project 2 repo

---
+++ Foisy_Christine_assignment2.js
@@ -0,0 +1,41 @@
+//Christine Foisy
+// SDI
+//Assignment 2
+
+
+var canIBeatMyTime = function(numObstacles,minPerObstacle){
+	var timeToBeat = 35;
+	var minPerObstacle = Math.floor(numObstacles/minPerObstacles);
+
+	if(time < 3.5){
+	
+		return minPerObstacle;
+			} else {
+				return 0;
+
+}; //ENDS canIBeatMyTime
+
+console.log(minPerobstacle);
+
+var minPerObstacle = timeToBeat (3.5,35);
+
+var minPerObstacle2 = timeToBeat (3.4,34);
+
+console.log ("the first obstacle took: " + minPerObstacle > "minPerObstacle.");
+
+console.log ("the second obstacle took: " + minPerObstacle2 > "minPerObstacle.");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+alert("JavaScript works!");}
