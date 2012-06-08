60af3340b21da8532af81b7e2115960feafe21aa
cfoisy1128 <cfoisy1128@yahoo.com>
2012-06-08 14:38:32 -0300

    Changes made to .js file

--- Foisy_Christine_assignment2.js
+++ Foisy_Christine_assignment2.js
@@ -25,6 +25,40 @@ console.log ("the first obstacle took: " + minPerObstacle > "minPerObstacle.");
 
 console.log ("the second obstacle took: " + minPerObstacle2 > "minPerObstacle.");
 
+var minutesPerObstacle = 3;
+	timeToBeat--; // timeTobeat = timeToBeat -1
+	
+
+var iwant = "to Win"
+	console.log("I want to beat my time")
+
+var obstacleNames = ["wall", "mountain", "tires", "hi/low crawl", "killer twins", "over and under", "beam", "sandbag carry", "log climb", "slide"];
+	console.log(obstacleNames["wall", "mountain", "tires", "hi/low crawl", "killer twins", "over and under", "beam", 	"sandbag carry", "log climb", "slide"] + " time to beat at each obstacle " + obstacleName + " for " + 					minutesPerObstacle + " I will go faster to beat my time!");
+
+            minutesPerObstacle = minutesPerObstacle[numObstacles];
+            
+            
+            
+            for (var minutes = 3.5; minutes < minutesPerObstacle; minutes -= 1) {
+                    var minutesRemain = minutesPerObstacle - minutes;
+                    
+                    
+             console.log("I beat my time by " + minutesRemain + " minutes left. " + minutes + " minutes have past.");
+
+
+};
+
+
+            console.log("I beat my time! Yippie!!" + obstacleName + "all");
+        };
+
+        for (var obstacleName = 3; minutesPerObstacle < minutesPerObstacle.length; obstacleName++) {
+            timeToBeat(obstacleName); 
+        };
+    
+
+
+
 
 
 
